05f84649bf8e3c3bb27efcb486db847d332e1be0
Tobius <>
2025-02-11 00:40:51 +0300

    news

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
+// index.js (for the plugin)
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Chats from './Chats';
+import { ThemeProvider, createTheme } from '@mui/material';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const theme = createTheme();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Export the initialization function that websites will call
+window.initAIChatPlugin = ({ agentId, containerId = 'ai-chat-root' }) => {
+  const container = document.getElementById(containerId);
+  if (container) {
+    const root = ReactDOM.createRoot(container);
+    root.render(
+      <ThemeProvider theme={theme}>
+        <Chats agentId={agentId} />
+      </ThemeProvider>
+    );
+  }
+};
